d79b9f3120690a4c9219c2c4ec2b0b74e5c784d3
Hiag0liveira <hiagophd@gmail.com>
2024-10-13 13:43:21 -0300

    fix: .env

--- src/main/java/com/hiagoliveira/portfolio/DemoMvcApplication.java
+++ src/main/java/com/hiagoliveira/portfolio/DemoMvcApplication.java
@@ -1,5 +1,6 @@
 package com.hiagoliveira.portfolio;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoMvcApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
+		System.out.println("DATABASE_URL: " + dotenv.get("DATABASE_URL"));
+		System.out.println("DATABASE_USERNAME: " + dotenv.get("DATABASE_USERNAME"));
+		System.out.println("DATABASE_PASSWORD: " + dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(DemoMvcApplication.class, args);
 	}
 }
