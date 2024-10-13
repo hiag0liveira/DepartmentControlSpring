9ca1cc6b0e9370e417a24fe28d67368753cdcb72
Hiag0liveira <hiagophd@gmail.com>
2024-10-13 18:33:39 -0300

    refactor: build mvn -N wrapper:wrapper

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,16 @@
-# Estágio 1: Construção do JAR usando Maven
-FROM maven:3.8.7-openjdk-17 AS build
+FROM ubuntu:latest AS build
 
-# Define o diretório de trabalho no container
-WORKDIR /app
+RUN apt-get update
+RUN apt-get install openjdk-17-jdk -y
+COPY . .
 
-# Copia o pom.xml e baixa dependências antecipadamente
-COPY pom.xml ./
-RUN mvn dependency:go-offline
+RUN apt-get install maven -y
+RUN mvn clean install
 
-# Copia todo o código-fonte para o container
-COPY src ./src
-
-# Compila e empacota a aplicação sem rodar os testes
-RUN mvn clean package -DskipTests
-
-# Estágio 2: Criação da imagem final leve
 FROM openjdk:17-jdk-slim
 
-# Define o diretório de trabalho no container
-WORKDIR /app
-
-# Expondo a porta 8080 para a aplicação
 EXPOSE 8080
 
-# Copia o JAR gerado do estágio de build para a imagem final
-COPY --from=build /app/target/demo-mvc-0.0.1-SNAPSHOT.jar app.jar
+COPY --from=build target/demo-mvc-0.0.1-SNAPSHOT.jar app.jar
 
-# Comando para iniciar a aplicação
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT [ "java", "-jar", "app.jar" ]
--- src/main/java/com/hiagoliveira/portfolio/DemoMvcApplication.java
+++ src/main/java/com/hiagoliveira/portfolio/DemoMvcApplication.java
@@ -8,14 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoMvcApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
-
-		System.out.println("DATABASE_URL: " + dotenv.get("DATABASE_URL"));
-		System.out.println("DATABASE_USERNAME: " + dotenv.get("DATABASE_USERNAME"));
-		System.out.println("DATABASE_PASSWORD: " + dotenv.get("DATABASE_PASSWORD"));
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+//		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+//		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+//
+//		System.out.println("DATABASE_URL: " + dotenv.get("DATABASE_URL"));
+//		System.out.println("DATABASE_USERNAME: " + dotenv.get("DATABASE_USERNAME"));
+//		System.out.println("DATABASE_PASSWORD: " + dotenv.get("DATABASE_PASSWORD"));
 
 		SpringApplication.run(DemoMvcApplication.class, args);
 	}
